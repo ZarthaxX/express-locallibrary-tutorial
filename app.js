1f664cfd33c63ddf2ddae2817a9aa51d4437733a
Tomas Tropea <tomastropea@hotmail.com>
2020-01-25 16:14:26 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://ZarthaxX:tejasrotas1@cluster0-pdetb.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://ZarthaxX:tejasrotas1@cluster0-pdetb.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
